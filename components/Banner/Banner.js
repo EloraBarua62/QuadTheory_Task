70335b7d73cce07b6266fe233752b0a837ba6a8a
EloraBarua <elorabarua62@gmail.com>
2024-01-05 21:58:56 +0600

    banner completed for mobile and tablet

--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -4,20 +4,24 @@ import banner from '../../public/Image1.png'
 
 const Banner = () => {
     return (
-      <div className="lg:w-[1068px] lg:h-[315px] mx-auto rounded-[28px] bg-amber-500 grid grid-cols-2">
+      <div className="w-full lg:w-[800px] xl:w-[1068px] xl:h-[315px] mx-auto rounded-[28px] md:bg-amber-500 grid grid-cols-1 md:grid-cols-2 gap-10">
         {/* Title */}
-        <div className="w-full flex justify-center items-center gap-6">
+        <div className="w-full flex justify-center items-center">
           <div>
-            <h1 className="text-4xl text-white font-semibold">
+            <h1 className="text-2xl sm:text-4xl md:text-white font-semibold text-center md:text-left">
               Deliver Food To Your <br /> Door Step!
             </h1>
-            <h1 className="text-gray-200 mt-6">
+            <h1 className="text-gray-600 md:text-gray-200 mt-2 md:mt-6">
               Authentic Food, Quick Service, Fast Delivery
             </h1>
           </div>
         </div>
-        <div className="w-full overflow-hidden">
-          <Image src={banner} alt="" className="object-cover"></Image>
+        <div className="w-full h-[250px] sm:h-[350px] md:h-full relative md:static md:overflow-hidden bg-orange-500 md:bg-transparent md:rounded-[28px]">
+          <Image
+            src={banner}
+            alt=""
+            className="w-full h-[280px] sm:h-[380px] md:h-full object-cover absolute md:static bottom-0"
+          ></Image>
         </div>
       </div>
     );
